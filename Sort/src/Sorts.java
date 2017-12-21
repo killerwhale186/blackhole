f06ae0ef6786366914f0fde90a43b4427759d4c4
Simon Du <sdu@xactlycorp.com>
2017-12-21 18:25:35 -0500

    fixes.

--- Sort/src/Sorts.java
+++ Sort/src/Sorts.java
@@ -4,27 +4,31 @@
 //  A collection of methods for sorting Arrays.
 //********************************************************************
 
-public class Sorts
-{
+public class Sorts {
 
 	private static int[] temp;
 
 	public static int Insertion(int[] list) {
-		int gcount = 0;
+		int icount = 0;
 
 		for (int n = 1; n < list.length; n++) {
 
 			// Save the next element to be inserted:
-			gcount++;
 			int listTemp = list[n];
 
 			// Going backward from list[n-1], shift elements to the
-			//   right until you find an element list[i] <= aTemp:
-			int i = n;
-			while (i > 0 && listTemp < list[i-1]) {
-				list[i] = list[i-1];
-				i--;
+			// right until you find an element list[i] <= aTemp:
 
+			int i = n;
+			while (i > 0 && listTemp < list[i - 1]) { // once the numbers
+														// compared are in the
+														// correct order least
+														// to greatest, end loop
+				list[i] = list[i - 1]; // moves to the left one if they aren't
+										// in the correct order least to
+										// greatest
+				i--;
+				icount++;
 			}
 
 			// Insert the saved element after a[i]:
@@ -32,47 +36,48 @@ public class Sorts
 
 			// Increment n (accomplished by n++ in the for loop).
 		}
-		System.out.print("Insertion Sort compares the pivot variable " + gcount + " times\n");
 
-		return 0;
+		return icount;
 	}
 
-
 	public static int Selection(int[] list) {
-		int gcount = 0;
+		int scount = 0;
 
 		for (int n = list.length; n > 1; n--) {
 			// Find the index iMax of the largest element
-			//   among list[0], ..., list[n-1]:
+			// among list[0], ..., list[n-1]:
+
 			int iMax = 0;
 			for (int i = 1; i < n; i++) {
-				if (list[i] > list[iMax])
+				if (list[i] > list[iMax]) // acending to decending switch
+											// between < and >
 					iMax = i;
-				gcount++;
+				scount++;
 			}
 
-			swap (list, iMax, n-1);
+			swap(list, iMax, n - 1);
 
-			// Decrement n (accomplished by n-- in the for loop).
+			// int listTemp = list[iMax];
+			// list[iMax] = list[n-1];
+			// list[n-1] = listTemp;
 		}
-		System.out.print("Selection Sort compares the pivot variable " + gcount + " times\n");
 
-		return 0;
+		return scount;
 	}
 
 	public static int Merge(int[] list, int from, int middle, int to) {
 
+		int mcount = 0;
 		temp = new int[list.length];
 		int i = from, j = middle + 1, k = from;
 
 		// While both arrays have elements left unprocessed:
 		while (i <= middle && j <= to) {
-
+			mcount++;
 			if (list[i] < list[j]) {
-				temp[k] = list[i];   // Or simply temp[k] = a[i++];
+				temp[k] = list[i]; // Or simply temp[k] = a[i++];
 				i++;
-			}
-			else {
+			} else {
 				temp[k] = list[j];
 				j++;
 			}
@@ -81,14 +86,14 @@ public class Sorts
 
 		// Copy the tail of the first half, if any, into temp:
 		while (i <= middle) {
-			temp[k] = list[i];     // Or simply temp[k++] = a[i++]
+			temp[k] = list[i]; // Or simply temp[k++] = a[i++]
 			i++;
 			k++;
 		}
 
 		// Copy the tail of the second half, if any, into temp:
 		while (j <= to) {
-			temp[k] = list[j];     // Or simply temp[k++] = a[j++]
+			temp[k] = list[j]; // Or simply temp[k++] = a[j++]
 			j++;
 			k++;
 		}
@@ -96,98 +101,95 @@ public class Sorts
 		// Copy temp back into a
 		for (k = from; k <= to; k++)
 			list[k] = temp[k];
-		return 0;
+
+		return mcount;
 	}
 
 	public static int mergeSort(int[] list, int from, int to) {
-		int gcount = 0;
+		int mcount = 0;
 
-		if (to - from < 2) {       // Base case: 1 or 2 elements 
+		if (to - from < 2) { // Base case: 1 or 2 elements
 			if (to > from && list[to] < list[from]) {
-				int listTemp = list[to];  // swap a[to] and a[from]
+				int listTemp = list[to]; // swap a[to] and a[from]
 				list[to] = list[from];
 				list[from] = listTemp;
 			}
-			//  System.out.print("Merge Sort compares the pivot variable " + gcount + " times\n");
-		}
-		else {      //recursive case
-			gcount++;
-			int middle = (from + to) / 2;
-			mergeSort(list, from, middle);
-			mergeSort(list, middle + 1, to);
-			Merge(list, from, middle, to);
-		}        
 
-		return 0;
+		} else { // recursive case
+			int middle = (from + to) / 2;
+			mcount += mergeSort(list, from, middle);
+			mcount += mergeSort(list, middle + 1, to);
+			mcount += Merge(list, from, middle, to);
+		}
+
+		return mcount;
 	}
 
-	public static int QuickSort(int[] list, int from, int to, int gcount) {
+	public static int QuickSort(int[] list, int from, int to) {
+		int qcount = 0;
 
 		if (from >= to) {
-			System.out.print("Quick Sort compares the pivot variable " + gcount + " times\n");
 			return 0;
 		}
 		// Choose pivot list[p]:
 		int p = from;
 		// The choice of the pivot location may vary:
-		//   you can also use p = from or p = to or use 
-		//   a fancier method, say, the median of the above three.
+		// you can also use p = from or p = to or use
+		// a fancier method, say, the median of the above three.
 
 		// Partition:
+
 		int i = from;
 		int j = to;
 		while (i <= j) {
 			if (list[i] <= list[p]) {
+				qcount++;
 				i++;
-				gcount++;
-			}
-			else if (list[j] >= list[p]) {
+			} else if (list[j] >= list[p]) {
+				//should increase by 2? 
+				qcount++;
 				j--;
-				gcount++;
-			}
-			else {
-				swap (list, i, j);
+			} else {
+				//should increase by 2? 
+				qcount++;
+				swap(list, i, j);
 				i++;
 				j--;
-				gcount++;
 			}
 		}
 
 		// Finish partitioning:
-		if (p < j) {   // place the pivot in its correct position
-			swap (list, j, p);
+		if (p < j) { // place the pivot in its correct position
+			swap(list, j, p);
 			p = j;
 		}
 
-		else if (p > i) {        
-			swap (list, i, p);
+		else if (p > i) {
+			swap(list, i, p);
 			p = i;
 		}
 
-		QuickSort(list, from, p - 1, gcount);
-		QuickSort(list, p + 1, to, gcount);
+		qcount += QuickSort(list, from, p - 1);
+		qcount += QuickSort(list, p + 1, to);
 
-		return 0;
+		return qcount;
 	}
 
 	public static int QuickMid(int[] list) {
-
-		QuickSort(list, 0, list.length / 2, 0);
 
 		return 0;
 	}
 
 	public static int QuickRandom(int[] list) {
 
-		QuickSort(list, 0, (int)( Math.random() * list.length), 0);
-
 		return 0;
 	}
 
-	private static void swap (int[] list, int a, int b) {
+	private static void swap(int[] list, int a, int b) {
 		int temp = list[a];
 		list[a] = list[b];
 		list[b] = temp;
 	}
 
 }
+
--- Sort/src/SortsLab.java
+++ Sort/src/SortsLab.java
@@ -1,61 +1,126 @@
+//********************************************************************
+//  Sortlab.java       Author:
+//
+//  Driver to exercise the use of several Sorting Arrays methods.
+//********************************************************************
 
-public class SortsLab
-{
-    static final int MAX=20;
-    
-    public SortsLab()
-    {
-        int[] list = new int[MAX], list2 = new int[MAX], list3 = new int[MAX], list4 = new int[MAX], list5 = new int[MAX];
-        int qcount=0, icount = 0, scount = 0, mcount = 0;
-            
-            
-        //Random Order List//////////////////
-        
-            System.out.println("Random order lists ");
-            ListSetup.MakeRandom (list);
-            ListSetup.Copy (list,list2);
-            ListSetup.Copy (list,list3);
-            ListSetup.Copy (list,list4);
-            ListSetup.Copy (list,list5);
-            
-        
-            System.out.println("\nList before sorting:");
-            ListSetup.Print (list);
-            
-            System.out.println("\nHere is the list after the Insertion Sort. ");
-            icount = Sorts.Insertion(list);
-            ListSetup.Print (list);        
-        
-            System.out.println("\nHere is the list after the Selection Sort. ");
-            scount = Sorts.Selection(list2);
-            ListSetup.Print (list2);
-            
-            System.out.println("\nHere is the list after the Quicksort (split first). ");
-            //qcount = Sorts.QuickSort(list3, 0, list.length - 1, 0);
-            qcount = Sorts.QuickMid(list3);
-            ListSetup.Print (list3);
-            
-            System.out.println("\nHere is the list after the Merge Sort. ");
-            mcount = Sorts.mergeSort(list4, 0, list.length - 1);
-            ListSetup.Print (list4);
-            
-            
-        //Ascending Order List//////////////////
-    
-            
-            
-            
-            
-    
-        //Descending Order List//////////////////
-    
-            
-            
-    }    
-    
-    public static void main(String args[])
-    {
-        
-        new SortsLab();
-    }
+public class SortsLab {
+	static final int MAX = 1000; // changes the array size
+
+	public SortsLab() {
+
+		int[] list = new int[MAX], list2 = new int[MAX], list3 = new int[MAX], list4 = new int[MAX],
+				list5 = new int[MAX];
+		int qcount = 0, icount = 0, scount = 0, mcount = 0;
+
+		// Random Order List//////////////////
+
+		System.out.println("Here are the sorts with arrays in random order:\n");
+		ListSetup.MakeRandom(list);
+		ListSetup.Copy(list, list2);
+		ListSetup.Copy(list, list3);
+		ListSetup.Copy(list, list4);
+		ListSetup.Copy(list, list5);
+
+		System.out.println("\nList before sorting:");
+		ListSetup.Print(list);
+
+		System.out.println("\nHere is the list after the Insertion Sort. ");
+		icount = Sorts.Insertion(list);
+		ListSetup.Print(list);
+		System.out.print("Insertion Sort compares the pivot variable " + icount + " times\n");
+
+		System.out.println("\nHere is the list after the Selection Sort. ");
+		scount = Sorts.Selection(list2);
+		ListSetup.Print(list2);
+		System.out.print("selection Sort compares the pivot variable " + scount + " times\n");
+
+		System.out.println("\nHere is the list after the Quicksort (split first). ");
+		qcount = Sorts.QuickSort(list3, 0, list.length - 1);
+		ListSetup.Print(list3);
+		System.out.print("quick Sort compares the pivot variable " + qcount + " times\n");
+
+		System.out.println("\nHere is the list after the Merge Sort. ");
+		mcount = Sorts.mergeSort(list4, 0, list.length - 1);
+		ListSetup.Print(list4);
+		System.out.print("merge Sort compares the pivot variable " + mcount + " times\n");
+
+		// Ascending Order List//////////////////
+		/*
+		System.out.print("\n\n\nHere are the sorts with arrays in ascending order:\n\n\n");
+		for (int n = list.length; n > 1; n--) {
+			int iMax = 0;
+			for (int i = 1; i < n; i++) {
+				if (list[i] > list[iMax])
+					iMax = i;
+			}
+			int listTemp = list[iMax];
+			list[iMax] = list[n - 1];
+			list[n - 1] = listTemp;
+		}
+		System.out.println("\nList before sorting (ascending):");
+		ListSetup.Print(list);
+
+		System.out.println("\nHere is the list after the Insertion Sort. ");
+		icount = Sorts.Insertion(list);
+		ListSetup.Print(list);
+		System.out.print("Insertion Sort compares the pivot variable " + icount + " times\n");
+
+		System.out.println("\nHere is the list after the Selection Sort. ");
+		scount = Sorts.Selection(list2);
+		ListSetup.Print(list2);
+		System.out.print("selection Sort compares the pivot variable " + scount + " times\n");
+
+		System.out.println("\nHere is the list after the Quicksort (split first). ");
+		qcount = Sorts.QuickSort(list3, 0, list.length - 1);
+		ListSetup.Print(list3);
+		System.out.print("quick Sort compares the pivot variable " + qcount + " times\n");
+
+		System.out.println("\nHere is the list after the Merge Sort. ");
+		mcount = Sorts.mergeSort(list4, 0, list.length - 1);
+		ListSetup.Print(list4);
+		System.out.print("merge Sort compares the pivot variable " + mcount + " times\n");
+
+		// Descending Order List//////////////////
+
+		System.out.print("\n\n\nHere are the sorts with arrays in decending order:\n\n\n");
+		for (int n = list.length; n > 1; n--) {
+			int iMax = 0;
+			for (int i = 1; i < n; i++) {
+				if (list[i] < list[iMax])
+					iMax = i;
+			}
+			int listTemp = list[iMax];
+			list[iMax] = list[n - 1];
+			list[n - 1] = listTemp;
+		}
+		System.out.println("\nList before sorting (decending):");
+		ListSetup.Print(list);
+
+		System.out.println("\nHere is the list after the Insertion Sort. ");
+		icount = Sorts.Insertion(list);
+		ListSetup.Print(list);
+		System.out.print("Insertion Sort compares the pivot variable " + icount + " times\n");
+
+		System.out.println("\nHere is the list after the Selection Sort. ");
+		scount = Sorts.Selection(list2);
+		ListSetup.Print(list2);
+		System.out.print("selection Sort compares the pivot variable " + scount + " times\n");
+
+		System.out.println("\nHere is the list after the Quicksort (split first). ");
+		qcount = Sorts.QuickSort(list3, 0, list.length - 1);
+		ListSetup.Print(list3);
+		System.out.print("quick Sort compares the pivot variable " + qcount + " times\n");
+
+		System.out.println("\nHere is the list after the Merge Sort. ");
+		mcount = Sorts.mergeSort(list4, 0, list.length - 1);
+		ListSetup.Print(list4);
+		System.out.print("merge Sort compares the pivot variable " + mcount + " times\n");
+		*/
+	}
+
+	public static void main(String args[]) {
+
+		new SortsLab();
+	}
 }
