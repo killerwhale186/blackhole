1d858b539748a675cc1a8b16f13a9fa7b5a0f79e
killerwhale186 <34631132+killerwhale186@users.noreply.github.com>
2018-12-10 17:01:01 -0500

    fix a bit

--- dark matter/src/org/galaxy/game/CalcTwentyFour.java
+++ dark matter/src/org/galaxy/game/CalcTwentyFour.java
@@ -13,16 +13,16 @@ import org.galaxy.math.FractionExpression;
 import org.galaxy.math.MathOperator;
 
 public class CalcTwentyFour {
-	
+
 	private static final Fraction TWENTY_FOUR = new Fraction(24);
-	
+
 	public static void main(String[] args) {
-		//testSpecial();
+		// testSpecial();
 		for (int i = 0; i < 10; i++) {
 			testRandom(4);
 		}
 	}
-	
+
 	private static void testSpecial() {
 		testOne(5, 6, 1, 4);
 		testOne(1, 6, 11, 13);
@@ -43,11 +43,11 @@ public class CalcTwentyFour {
 		testOne(5, 5, 7, 11);
 		testOne(5, 7, 7, 11);
 	}
-	
+
 	private static void testRandom(int n) {
 		List<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
-			list.add((int)(1 + Math.random() * 13));
+			list.add((int) (1 + Math.random() * 13));
 		}
 		String ans = solve(list);
 		System.out.println(list + " ===>>> " + ans);
@@ -62,53 +62,50 @@ public class CalcTwentyFour {
 		String ans = solve(list);
 		System.out.println(list + " ===>>> " + ans);
 	}
-	
+
 	private static String solve(List<Integer> list) {
-		
-		//prepare the initial data
+
+		// prepare the initial data
 		Map<String, List<FractionExpression>> mp = new HashMap<String, List<FractionExpression>>();
-		List<FractionExpression> f0 = new ArrayList<FractionExpression>();
+		List<FractionExpression> fe = new ArrayList<FractionExpression>();
 		for (Integer n : list) {
-			f0.add(new Fraction(n));
+			fe.add(new Fraction(n));
 		}
-		mp.put(getKey(f0), f0);
-		
+		mp.put(getKey(fe), fe);
+
 		int s = list.size();
 		while (s > 1) {
+			List<int[]> subIndex = getSubIndex(s);
 			Map<String, List<FractionExpression>> mp2 = new HashMap<String, List<FractionExpression>>();
+			
 			for (List<FractionExpression> currList : mp.values()) {
-				//pick any two
-				for (int i = 0; i < s-1; i++) {
-					for (int j = i + 1; j < s; j++) {
-
-						List<FractionExpression> remains = getRemainItems(currList, s, i, j);
-						List<FractionExpression> combo = getAllCombo(currList, i, j);
-
-						for (FractionExpression f : combo) {
-							List<FractionExpression> l2 = new ArrayList<FractionExpression>();
-							l2.add(f);
-							l2.addAll(remains);
-							mp2.put(getKey(l2), l2);
-						}
+				for (int[] index : subIndex) {
+					List<FractionExpression> remains = getRemainItems(currList, s, index[0], index[1]);
+					List<FractionExpression> combo = getAllCombo(currList, index[0], index[1]);
+					for (FractionExpression f : combo) {
+						List<FractionExpression> l2 = new ArrayList<FractionExpression>();
+						l2.add(f);
+						l2.addAll(remains);
+						mp2.put(getKey(l2), l2);
 					}
 				}
 			}
-			
+
 			mp = mp2;
 			s--;
 		}
-		
-		//at this point, we have a list of all possible values (FractionExpression)
+
+		// at this point, we have a list of all possible values (FractionExpression)
 		for (List<FractionExpression> currList : mp.values()) {
 			FractionExpression f = currList.get(0);
 			if (f.getValue().equals(TWENTY_FOUR)) {
 				return f.toString();
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	private static List<FractionExpression> getRemainItems(List<FractionExpression> lst, int s, int i, int j) {
 		List<FractionExpression> remains = new ArrayList<FractionExpression>();
 		for (int k = 0; k < s; k++) {
@@ -118,11 +115,11 @@ public class CalcTwentyFour {
 		}
 		return remains;
 	}
-	
+
 	private static List<FractionExpression> getAllCombo(List<FractionExpression> lst, int i, int j) {
 		FractionExpression f1 = lst.get(i);
 		FractionExpression f2 = lst.get(j);
-		
+
 		List<FractionExpression> list = new ArrayList<FractionExpression>();
 		list.add(new CompositeExpression(f1, f2, MathOperator.PLUS));
 		list.add(new CompositeExpression(f1, f2, MathOperator.MINUS));
@@ -137,13 +134,23 @@ public class CalcTwentyFour {
 		return list;
 	}
 
-	//used to eliminate duplicates
+	// used to eliminate duplicates
 	private static String getKey(List<FractionExpression> ff) {
 		Collections.sort(ff, new FractionComparator());
 		StringBuffer buf = new StringBuffer();
-		for (FractionExpression f: ff) {
+		for (FractionExpression f : ff) {
 			buf.append("{" + f.getValue() + "}");
 		}
 		return buf.toString();
+	}
+
+	private static List<int[]> getSubIndex(int k) {
+		List<int[]> ret = new ArrayList<int[]>();
+		for (int i = 0; i < k - 1; i++) {
+			for (int j = i + 1; j < k; j++) {
+				ret.add(new int[]{i, j});
+			}
+		}
+		return ret;
 	}
 }
